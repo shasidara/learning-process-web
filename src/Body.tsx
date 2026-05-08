a78c343590e76a47c8031b3d2a86c3dc48e64e16
Your Name <you@example.com>
2026-05-08 16:54:07 +0530

    Routing

---
+++ src/Body.tsx
@@ -0,0 +1,16 @@
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+const Body = () => {
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+            <Footer /> 
+        </>
+        
+    );
+};
+
+export default Body;
